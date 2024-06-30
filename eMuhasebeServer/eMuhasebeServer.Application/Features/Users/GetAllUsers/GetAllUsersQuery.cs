c20c153925e13c277e15b3550c7f3a69b4c625bc
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-30 13:05:53 +0300

    user crud

---
+++ eMuhasebeServer/eMuhasebeServer.Application/Features/Users/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,8 @@
+﻿
+
+using eMuhasebeServer.Domain.Entities;
+using MediatR;
+using TS.Result;
+
+namespace eMuhasebeServer.Application.Features.Users.GetAllUsers;
+internal sealed record GetAllUsersQuery() : IRequest<Result<List<AppUser>>>;
